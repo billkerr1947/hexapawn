0b92b4499105cfbaf78e6cd20f9f32556cb3f27f
Bill Kerr <billkerr@gmail.com>
2025-06-05 09:05:15 +0930

    Screen bg colour changed

--- hexapawn.py
+++ hexapawn.py
@@ -1,15 +1,20 @@
 import pygame
+import sys  # to exit
 
-pygame.init()
-screen = pygame.display.set_mode((300, 300))
+pygame.init()   # initialise pygame modules
+screen = pygame.display.set_mode((300, 300))    #create screen surface
 pygame.display.set_caption('Hexapawn')
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # create a clock
+bg_colour = (230,230,230)   #bg colour value
 
-while True:
-    pygame.display.update()
+while True:    
     for event in pygame.event.get():
+        # any keyboard or mouse event will activate this loop
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-            
+            # pygame.quit()
+            sys.exit()
+    
+    screen.fill(bg_colour)  
+    pygame.display.flip()   # updates entire display, must come afer fill(bg_colour)
     clock.tick(60)
+    
